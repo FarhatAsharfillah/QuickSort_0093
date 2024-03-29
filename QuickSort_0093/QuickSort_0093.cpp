e11e5a0c0e08400155965138513f5ed1456419ac
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-03-29 16:01:46 +0700

    commit 17 input for

--- QuickSort_0093/QuickSort_0093.cpp
+++ QuickSort_0093/QuickSort_0093.cpp
@@ -97,3 +97,8 @@ void display() {
     cout << "\n----------" << endl;
     cout << "Sorted Array" << endl;
     cout << "------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
